b805c7e438def0c4c441de7c2f4bd910586c6891
Narasimha-png <narasimhas9490@gmail.com>
2024-11-25 16:28:14 +0530

    Version 1.2

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,20 @@
 import "../Styles/Login.css" ;
 import Introduction from "../Assets/intro.png" ;
 import { useState } from "react";
+import Hideimg from "../Assets/hide.png" ;
 import { useNavigate } from "react-router-dom";
 const Ellipse = ()=>(
     <div className="blue-ellipse">
     </div>
+)
+const HideEye = ({visible,setVisible})=>(
+    <img  src={Hideimg} 
+    onClick={(e)=>{
+        setVisible(!visible) ;
+    }}
+    style={{
+        display:visible?"revert":"none" 
+    }} className="HideImg"></img>
 )
 const Intro = ()=> (
     <div className="intro-div">
@@ -32,8 +42,11 @@ const LogInput = ()=> {
         
          setMaskedPwd("*".repeat(pwd.length+1) ) ;
     }
+   
     return(
+     
     <div className="login-inputs">
+       
         <p className="login-text"  >Join the Fun</p>
     <input onInput={(e)=>{
         setUserName(e.target.value) ;
@@ -56,12 +69,18 @@ const LogInput = ()=> {
         HandleInputPwd(e);
         
         }} value={visible ? pwd : maskedPwd }></input>
+         {visible && <HideEye visible={visible} setVisible={setVisible}   />} 
+      
     <svg onClick={(e)=>{
+        
         setVisible(!visible) ;
-    }} className="eye-symbol" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+       
+    }
+   
+    }  className="eye-symbol" style={{display:visible?"none":"revert"}} viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M9 6.75C9.59674 6.75 10.169 6.98705 10.591 7.40901C11.0129 7.83097 11.25 8.40326 11.25 9C11.25 9.59674 11.0129 10.169 10.591 10.591C10.169 11.0129 9.59674 11.25 9 11.25C8.40326 11.25 7.83097 11.0129 7.40901 10.591C6.98705 10.169 6.75 9.59674 6.75 9C6.75 8.40326 6.98705 7.83097 7.40901 7.40901C7.83097 6.98705 8.40326 6.75 9 6.75ZM9 3.375C12.75 3.375 15.9525 5.7075 17.25 9C15.9525 12.2925 12.75 14.625 9 14.625C5.25 14.625 2.0475 12.2925 0.75 9C2.0475 5.7075 5.25 3.375 9 3.375ZM2.385 9C2.99119 10.2377 3.93248 11.2806 5.10186 12.0099C6.27124 12.7393 7.6218 13.126 9 13.126C10.3782 13.126 11.7288 12.7393 12.8981 12.0099C14.0675 11.2806 15.0088 10.2377 15.615 9C15.0088 7.76227 14.0675 6.71944 12.8981 5.99006C11.7288 5.26068 10.3782 4.87401 9 4.87401C7.6218 4.87401 6.27124 5.26068 5.10186 5.99006C3.93248 6.71944 2.99119 7.76227 2.385 9Z" fill="#4592EC"/>
    
- </svg>
+ </svg> 
   
     </div>
     <div className="error-msg">{error.length > 0 ? error : ""}</div>
@@ -91,9 +110,12 @@ const LogInput = ()=> {
 }
 const Login = () => (
     <>
+    <div className="login-main-div">
     <Ellipse />
     <Intro />
+    </div>
     <LogInput />
     </>
+    
 )
 export default Login ;
